f562b0f6034c56a1b7935086ea06edfbb5b3a09b
Chinmay Patil <chinmaydp3006@gmail.com>
2026-09-05 17:24:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Set Bits/count-set-bits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int setBits(int n) {
+        // code here
+        int count = 0;
+        while(n != 0) {
+            if((n & 1) != 0){
+                count++;
+                n = n >> 1;
+            } else{
+                n = n >> 1;
+            }
+        }
+        return count;
+    }
+}
